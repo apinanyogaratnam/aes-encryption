8885cb2f433fc393f8659d0697c5f02d1935741b
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-26 16:08:45 -0500

    build: updated setup to match project metadata

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="base-package-name",
+    name="aes-encryption",
     version="0.1.0",
     author="apinanyogaratnam",
     author_email="apinanapinan@icloud.com",
-    description="A base package for Python",
+    description="An AES encryption package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/apinanyogaratnam/base-python-package-template",
+    url="https://github.com/apinanyogaratnam/aes-encryption",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,5 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10.4",
-    install_requires=[],
+    install_requires=[
+        "pycryptodome",
+    ],
 )
