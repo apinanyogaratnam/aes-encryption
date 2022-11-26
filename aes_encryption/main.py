3cfefa06ba1d0f9aec2d408a779753cf60022616
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-26 12:53:34 -0500

    feat: created AESCipher class

---
+++ aes_encryption/main.py
@@ -0,0 +1,28 @@
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+    def __init__(self: 'AESCipher', key: str) -> None:
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self: "AESCipher", raw: str) -> str:
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode())).decode()
+
+    def decrypt(self: 'AESCipher', enc: bytes) -> str:
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self: 'AESCipher', s: str) -> str:
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s: str) -> str:
+        return s[:-ord(s[len(s)-1:])]
