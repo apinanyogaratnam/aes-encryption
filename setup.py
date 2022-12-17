d1c2ea0fa708e657eb6267a7c285ad6fb9b23ebf
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-16 21:06:19 -0500

    chore: updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="aes-encryption",
-    version="0.1.0",
+    version="0.2.0",
     author="apinanyogaratnam",
     author_email="apinanapinan@icloud.com",
     description="An AES encryption package",
